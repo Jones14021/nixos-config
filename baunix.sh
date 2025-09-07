c64d70d98705a65c9edcc053fb59467b135450c5
HOERMANN Jonas <jonashoermann12@gmail.com>
2025-09-07 21:02:44 +0200

    add baunix script

---
+++ baunix.sh
@@ -0,0 +1,3 @@
+label="$(git -C ~/nixos-config log -1 --pretty=%s)"
+# https://search.nixos.org/options?channel=25.05&show=system.nixos.label
+sudo env NIXOS_LABEL="$label" nixos-rebuild switch --flake "$HOME/nixos-config#$(hostname)"
