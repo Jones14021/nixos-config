8b8374fed94846295f782f7f3d089c083837d07d
HOERMANN Jonas <jonashoermann12@gmail.com>
2025-09-14 16:22:21 +0200

    rebuild KDE apps cache after nix build

--- baunix.sh
+++ baunix.sh
@@ -1,3 +1,6 @@
 label="$(git -C ~/nixos-config log -1 --pretty=%s)"
 # https://search.nixos.org/options?channel=25.05&show=system.nixos.label
 sudo env NIXOS_LABEL="$label" nixos-rebuild switch --flake "$HOME/nixos-config#$(hostname)"
+
+# Rebuild KService/KSycoca cache fully
+kbuildsycoca6 --noincremental
