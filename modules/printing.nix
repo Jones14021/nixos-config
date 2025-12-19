415de534157afff56560ae1ab36a85b9737c76d3
HOERMANN Jonas <jonashoermann12@gmail.com>
2025-12-19 13:56:09 +0100

    add printer 'HP OfficeJet 3830'

--- modules/printing.nix
+++ modules/printing.nix
@@ -1,3 +1,27 @@
+# https://nixos.wiki/wiki/Printing
+#
+# tips:
+#
+# find printer device URIs:
+#   lpinfo -v
+# 
+# check whether CUPS knows a driver/printer model:
+#   lpinfo -m | grep -i -E 'officejet.*3830|3830'
+#
+# Use the CUPS web UI and pick the model there (it often exposes more options than KDE):
+#   http://localhost:631 → Administration → Add Printer
+#
+# test IPP printing:
+#   TEST="$(nix eval --raw nixpkgs#cups.outPath)/share/cups/ipptool/get-printer-attributes.test"
+#   ipptool -tv ipp://192.168.178.78:631/ipp/print "$TEST"
+#
+#   or more like mDNS output:
+#
+#   ipptool -tv ipp://192.168.178.78:631/ipp/print "$TEST" \
+#     | grep -E "printer-name|printer-make-and-model|printer-uri-supported|ipp-versions-supported|document-format-supported"
+
+
+
 { pkgs, lib, ... }:
 
 {
@@ -34,6 +58,17 @@
         ppdOptions = {
             PageSize = "A4";
             Duplex = "DuplexNoTumble";  # Enables double-sided printing with long edge binding
+        };
+    }
+    {
+        name        = "HP_OfficeJet_3830";
+        description = "HP_OfficeJet_3830";
+        location    = "Eppisburg";
+        deviceUri   = "ipp://192.168.178.78/ipp/print";
+        # From `lpinfo -m`:
+        model       = "HP/hp-officejet_3830_series.ppd.gz";
+        ppdOptions = {
+            PageSize = "A4";
         };
     }
     ];
