eb94f71ae710a04981d68f9d77115b0f2f9ef999
HOERMANN Jonas <jonashoermann12@gmail.com>
2026-03-07 23:51:50 +0100

    add Dolphin bookmark for OTH user home mount

--- modules/home-manager/mounts.nix
+++ modules/home-manager/mounts.nix
@@ -4,7 +4,11 @@ let
   gio = "${pkgs.glib.bin}/bin/gio";
   user = "hoj43157";
   password = "YOUR_STRONG_PASSWORD"; # TODO: securely handle this
-  address = "smb://fs.hs-regensburg.de/storage/home/${user}";
+  server = "fs.hs-regensburg.de";
+  share = "storage/Home";
+  address = "smb://${server}/${share}/${user}";
+  # The URI path Dolphin uses for GVfs mounts
+  gvfsUri = "file:///run/user/1000/gvfs/smb-share:server=${server},share=${share}/${user}";
 in {
   # We do not need manual daemons (gvfs-daemon, gvfsd-fuser);
   # NixOS dbus activation handles it now.
@@ -60,4 +64,36 @@ in {
   home.sessionVariables = {
     GIO_EXTRA_MODULES = "\${GIO_EXTRA_MODULES}\${GIO_EXTRA_MODULES:+:}${pkgs.gvfs}/lib/gio/modules";
   };
+
+  # Use a Home-Manager activation script to inject the bookmark into Dolphin's Places
+  home.activation.addOthDolphinBookmark = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    PLACES_FILE="$HOME/.local/share/user-places.xbel"
+    
+    # Check if the file exists (KDE creates it on first run, but we create a basic one if missing)
+    if [ ! -f "$PLACES_FILE" ]; then
+      mkdir -p "$HOME/.local/share"
+      echo '<?xml version="1.0" encoding="UTF-8"?>' > "$PLACES_FILE"
+      echo '<!DOCTYPE xbel>' >> "$PLACES_FILE"
+      echo '<xbel xmlns:bookmark="http://www.freedesktop.org/standards/desktop-bookmarks" xmlns:mime="http://www.freedesktop.org/standards/shared-mime-info">' >> "$PLACES_FILE"
+      echo '</xbel>' >> "$PLACES_FILE"
+    fi
+
+    # Check if the bookmark already exists to avoid duplicates
+    if ! grep -q "${gvfsUri}" "$PLACES_FILE"; then
+      # Insert the bookmark right before the closing </xbel> tag using sed
+      sed -i '/<\/xbel>/i \
+      <bookmark href="${gvfsUri}">\n\
+        <title>${user}</title>\n\
+        <info>\n\
+          <metadata owner="http://freedesktop.org">\n\
+            <bookmark:icon name="network-server"/>\n\
+          </metadata>\n\
+          <metadata owner="http://www.kde.org">\n\
+            <ID>1</ID>\n\
+            <isSystemItem>false</isSystemItem>\n\
+          </metadata>\n\
+        </info>\n\
+      </bookmark>' "$PLACES_FILE"
+    fi
+  '';
 }
