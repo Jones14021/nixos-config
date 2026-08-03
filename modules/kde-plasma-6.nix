009f6c1ab57016eeb402d1de3827126b0067ae12
HOERMANN Jonas <jonashoermann12@gmail.com>
2026-08-03 15:10:01 +0200

    switch display manager to plasma-login-manager

--- modules/kde-plasma-6.nix
+++ modules/kde-plasma-6.nix
@@ -2,9 +2,8 @@
 
 {
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.plasma-login-manager.enable = true;
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.autoNumlock = true;
   services.xserver.xkb = { layout = "de"; variant = ""; };
   console.keyMap = "de";
 
